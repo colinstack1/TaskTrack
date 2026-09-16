87520d08bff7beb0e654bce3838efb9985ddd3a8
Colin Stack <colinstackaa@gmail.com>
2026-09-16 14:22:49 -0400

    Creating view_task function

--- tasktrack.py
+++ tasktrack.py
@@ -24,8 +24,18 @@ def add_task(tasks):
 
 def view_tasks(tasks):
     """Display all tasks currently stored in the task list."""
-    #TODO: Complete this function in Step 9.
-    pass
+    if not tasks:
+        #TODO: Display the empty-list message.
+        print("No tasks found.")
+        return
+    
+
+    print("\nTasks:")
+
+    #TODO: Loop through enumerate(tasks, start=1).
+    #TODO: Display each number and task using an f-string.
+    for number, task in enumerate(tasks, start=1):
+        print(f"{number}. {task}")
 
 
 def main():
