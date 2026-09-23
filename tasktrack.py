9cd14f73cc6b2f3851ea0174fd36834f3056708f
Colin Stack <colinstackaa@gmail.com>
2026-09-22 22:11:20 -0400

    Save tasks between program runs

--- tasktrack.py
+++ tasktrack.py
@@ -56,6 +56,17 @@ def load_tasks(filename):
         return []
 
     return tasks
+
+
+def save_tasks(tasks, filename):
+    """Save all tasks to a text file."""
+    with open(filename, "w") as file:
+        #TODO: Loop through the tasks list.
+        for task in tasks:
+        #TODO: Write each task followed by a newline character.
+            file.write(f"{task}\n")
+
+
 def main():
     """Run the TaskTrack menu until the user chooses to exit."""
     tasks = load_tasks(TASKS_FILE)
@@ -68,6 +79,7 @@ def main():
             view_tasks(tasks)
         elif choice == "2":
             add_task(tasks)
+            save_tasks(tasks, TASKS_FILE)
         elif choice == "3":
             print("Goodbye!")
             break
